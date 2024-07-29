43d1dcec6b2f14f65930477887b7d81fd3d9d23d
Omkar Jadhav <objadhav28@gmail.com>
2024-07-29 21:17:05 +0530

    commit

--- src/com/project/exception/ResourceNotFoundException.java
+++ src/com/project/exception/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package com.project.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
